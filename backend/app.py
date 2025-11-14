e8a30eac69130f558cc2547b77c4eb1642af2977
jadhav.m <jadhav.m@ufl.edu>
2025-11-14 11:16:10 -0500

    Update start end default

--- backend/app.py
+++ backend/app.py
@@ -125,7 +125,7 @@ def predict_trajectory():
                 }), 404
         else:
             # Use first user if none specified
-            user_id = list(generator.user_transitions.keys())[1]
+            user_id = list(generator.user_transitions.keys())[17]
             logger.info(f"No user_id provided, using default user: {user_id}")
         
         # Generate trajectory
